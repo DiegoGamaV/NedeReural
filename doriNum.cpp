c05a9dab1aeaf3f54f490529ec19a604d53ffe0b
gabrielfelipeg <gabrielfelipedoritos@gmail.com>
2018-09-11 16:01:41 -0300

    Adding custom numpy library in cpp

---
+++ doriNum.cpp
@@ -0,0 +1,102 @@
+#include "doriNum.h"
+
+Narray::Narray(unsigned int _row, unsigned int _colunm){
+    values = (double**) malloc(sizeof(double*) * _row);
+    for(register int i = 0; i < _row; i++)
+        values[i] = (double*) malloc(sizeof(double) * _colunm);
+    row = _row;
+    colunm = _colunm;
+    randomValues();
+}
+
+Narray Narray::operator+ (const double &a){
+    for(register int i = 0; i < row; i++){
+        for(register int j = 0; j < colunm; j++){
+            values[i][j] += a;
+        }
+    }
+}
+
+Narray Narray::operator- (const double &a){
+    for(register int i = 0; i < row; i++){
+        for(register int j = 0; j < colunm; j++){
+            values[i][j] -= a;
+        }
+    }
+}
+
+Narray Narray::operator* (const double &a){
+    for(register int i = 0; i < row; i++){
+        for(register int j = 0; j < colunm; j++){
+            values[i][j] *= a;
+        }
+    }
+}
+
+Narray Narray::operator/ (const double &a){
+    for(register int i = 0; i < row; i++){
+        for(register int j = 0; j < colunm; j++){
+            values[i][j] /= a;
+        }
+    }
+}
+
+Narray Narray::operator+ (const Narray &a){
+    if(a.row != row || a.colunm != colunm){
+        exit(1);
+    }
+    Narray ret = Narray(row, colunm);
+    for(register int i = 0; i < row; i++){
+        for(register int j = 0; j < colunm; j++){
+            ret.values[i][j] = a.values[i][j] + values[i][j];
+        }
+    }
+    return ret;
+}
+Narray Narray::operator- (const Narray &a){
+    if(a.row != row || a.colunm != colunm){
+        exit(1);
+    }
+    Narray ret = Narray(row, colunm);
+    for(register int i = 0; i < row; i++){
+        for(register int j = 0; j < colunm; j++){
+            ret.values[i][j] = a.values[i][j] - values[i][j];
+        }
+    }
+    return ret;
+}
+
+Narray Narray::operator* (const Narray &a){
+    if(colunm != a.row){
+        exit(1);
+    }
+    Narray ret = Narray(row, a.colunm);
+    memset(ret.values, 0, sizeof ret.values);
+    for(register int i = 0; i < row; i++){
+        for(register int j = 0; j < a.colunm; j++){
+            for(register int k = 0; k < colunm; k++){
+                ret.values[i][j] += values[i][k] * a.values[k][j];
+            }
+        }
+    }
+}
+
+void Narray::randomValues(){
+    srand(time(NULL));
+    for(register int i = 0; i < row; i++){
+        for(register int j = 0; j < colunm; j++){
+            values[i][j] = rand() / ((double) RAND_MAX);
+        }
+    }
+}
+
+int main(){
+    Narray n = Narray(2, 2);
+    n.randomValues();
+    for(int i = 0; i < 2; i++){
+        for(int j = 0; j < 2; j++){
+            std::cout << n.values[i][j] << " \n"[j + 1 == 2];
+        }
+    }
+    return 0;
+}
---
+++ doriNum.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <bits/stdc++.h>
+
+struct Narray{
+    double** values;
+    unsigned int row, colunm;
+
+    Narray(unsigned int _row, unsigned int _colunm);
+
+    Narray operator+ (const double &a);
+
+    Narray operator- (const double &a);
+
+    Narray operator* (const double &a);
+
+    Narray operator/ (const double &a);
+
+    Narray operator+ (const Narray &a);
+
+    Narray operator- (const Narray &a);
+
+    Narray operator* (const Narray &a);
+
+    void randomValues();
+};
