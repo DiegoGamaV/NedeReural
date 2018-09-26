f1eb908ff066fe4178d5b3f82c7e4ec59be74e5e
Thúlio Carvalho <thulioicc@gmail.com>
2018-09-25 23:04:06 -0300

    added naive main code

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,68 @@
-#include "inputreader.h"
-#include "output.h"
-#include "network.h"
+#include "netFlow.h"
 
-int main(){
+enum Type
+{
+    TRAIN,
+    EXEC
+};
+
+
+void printUsage();
+Type readInput(std::string str);
+void treatInvalidInput(std::string str);
+
+int main(int argc, char* argv[]){
+
+    if (argc < 2) {
+        printUsage();
+        exit(1);
+    }
+
+    std::string str(argv[1]);
+    treatInvalidInput(str);
+    Type type = readInput(str);
+    
+    switch (type)
+    {
+        case TRAIN:            
+            /* Codigo para treinamento da rede */
+            break;
+            
+        case EXEC:
+            /* Codigo para execucao normal da rede */
+            break;
+
+        default:
+            break;
+    }
+
     return 0;
+}
+
+Type readInput(std::string str) {
+    Type typeOfExecution;
+
+    if (str == "--exec") {
+        typeOfExecution = EXEC;
+    } else if (str == "--train") {
+        typeOfExecution = TRAIN;
+    }
+
+    return typeOfExecution;
+}
+
+void treatInvalidInput(std::string str){
+    bool error = false;
+    if ((str != "--exec") && (str != "--train")) {
+        error = true;
+    }
+    if (error) {
+        printUsage();
+        exit(1);
+    }
+}
+
+void printUsage() {
+    std::cout << "Usage:" << std::endl;
+    std::cout << "  ./run (--train | --exec)" << std::endl;
 }
