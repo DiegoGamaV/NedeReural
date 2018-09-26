0be8b61a5a9a4c8c7340486bede3ef7950ebd905
Thúlio Carvalho <thulioicc@gmail.com>
2018-09-25 23:41:50 -0300

    added naive netFlow structure

--- src/netFlow.h
+++ src/netFlow.h
@@ -1,3 +1,12 @@
 #pragma once
 #include "network.h"
 #include "inputreader.h"
+
+// Funcao que executa a rede neural
+// apenas para fornecer uma resposta
+void execute();
+
+
+// Funcao que executa a rede neural no
+// modo de treinamento
+void train();
